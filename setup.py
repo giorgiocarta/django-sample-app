cb8a2a60b35d6704246a98327825651d22f1fd0f
donedeal-giorgio <giorgio@donedeal.ie>
2020-03-30 16:11:59 +0100

    setup file

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='django_app',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='giorgiocarta',
+    install_requires=[
+        "asgiref==3.2.3",
+        "Django==3.0.3",
+        "django-crispy-forms==1.8.1",
+        "django-environ==0.4.5",
+        "django-fsm==2.7.0",
+        "django-fsm-admin==1.2.4",
+        "Pillow==7.0.0",
+        "pytz==2019.3",
+        "sqlparse==0.3.0",
+        "psycopg2==2.8.4"
+    ],
+    author_email='',
+    description=''
+)
